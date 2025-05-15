eeb199502e99e8c8f04fe4d6458db1784887b134
itai-fuchs <itaifuchs@gmail.com>
2025-05-15 12:13:20 +0300

    Update Program.cs
    finish

--- atbash/atbash/Program.cs
+++ atbash/atbash/Program.cs
@@ -70,6 +70,38 @@ namespace atbash
         }
 
     }
+
+     static void warning_message(Func<string, string, Dictionary<string, string>> func, string arg1, string arg2)
+        {
+
+            Dictionary<string, string> dict = func(arg1, arg2);
+            int points = Int16.Parse(dict["points"]);
+            if (points >= 1 && points <= 5)
+            {
+
+                dict["message"] += " WARNING";
+                Console.WriteLine($"the message is: {dict["message"]}\nthe decripted message is: {dict["decripted_message"]}\n" +
+                    $"the total points is: {dict["points"]}");
+
+            }
+            else if (points >= 6 && points <= 10)
+            {
+                dict["message"] += "DANGER";
+                Console.WriteLine($" the message is: {dict["message"]}\nthe decripted message is: {dict["decripted_message"]}\n" +
+                    $"the total points is: {dict["points"]}");
+            }
+            else if (points >= 11 && points <= 15)
+            {
+                dict["message"] += "ULTRA AlErT";
+                Console.WriteLine($" the message is: {dict["message"]}\nthe decripted message is: {dict["decripted_message"]}\n" +
+                    $"the total points is: {dict["points"]}");
+            }
+        }
+
+      
+
+
+    }
 }
 
 
